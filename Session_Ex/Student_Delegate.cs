929cf19185e5afaaeb553411e0591d55b80d6aae
PrashantSuryawanshi <suryawanshiprashant026@gmail.com>
2022-03-23 19:18:44 +0530

    Add project files.

---
+++ Session_Ex/Student_Delegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_Ex
+{
+    public class Student_Delegate
+    {
+        //Event
+        public event DisplayMsg FailEvent;  //Marks less then 40
+        public event DisplayMsg PassEvent; //Marks greter then 40
+        public void AcceptMarks(int marks)
+        {
+            if (marks >=40)
+            {
+                PassEvent(); //call to event or raise an event
+            }
+            else
+            {
+                FailEvent();//call to event or raise an event
+            }
+        }
+    }
+}
